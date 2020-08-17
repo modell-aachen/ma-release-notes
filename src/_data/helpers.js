2aeb16b81fe09f9f12c1e5fd720d3e488f4657bf
Martin Schmitz <schmitz@modell-aachen.de>
2020-08-17 13:43:36 +0200

    Improves Reading Time (#24)
    1 Minute is now correctly shown instead of 1 Minutes

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -6,5 +6,10 @@ module.exports = {
     const wordsPerMinute = 200;
     const numberOfWords = text.split(/\s/g).length;
     return Math.ceil(numberOfWords / wordsPerMinute);
+  },
+  getReadingTimeString(text) {
+    const numberOfMinutes = this.getReadingTime(text);
+    const whichMinute = numberOfMinutes === 1 ? "Minute" : "Minuten";
+    return `${numberOfMinutes} ${whichMinute}`;
   }
 };
