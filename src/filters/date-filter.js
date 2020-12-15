8d984d646ad108ad2e395161f4f518a5a4d1e2d8
Martin Schmitz <schmitz@modell-aachen.de>
2020-12-15 15:46:02 +0100

    Release tooltips (#79)
    * creates partial for release tags
    
    * Adds explanation tooltip. Some cleanup work.
    
    * Adds link to wartungsplanung

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -2,8 +2,9 @@
 
 module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
-
-  const months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
-
-  return `${dateObject.getDate()}. ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+  return dateObject.toLocaleString("de-DE", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
 };
